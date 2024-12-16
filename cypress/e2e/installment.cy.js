9e4976808ae4bee9b9dc43803cb26dc0f679f037
Marcos Colossi <colossim@gmai.com>
2024-12-15 21:15:46 -0300

    Renomeia installment.cy.js para installment-test.cy.js

---
+++ cypress/e2e/installment.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Verificar dos produtos na página do eccomerce', () => {
+    
+    it('Deve verificar se todas sd psrcelsa dos produtos estão visíveis', () => {
+        cy.get('.installment').each(($el, index) => {
+        cy.wrap($el).should('be.visible'); // Verifica se valar formatado de cada parcela dos produtos está visível
+        cy.screenshot(`produto_imagem_${index + 1}`); // Captura de tela do valor formatado das parcelas de cada produto
+      })
+    })
+
+})
